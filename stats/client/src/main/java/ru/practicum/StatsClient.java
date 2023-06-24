7eb82ad11f106c4b1e660ae5f5c58636aba9b925
Константин <k.yazykov@bk.ru>
2023-06-24 11:02:05 +0300

    Diplom: "2. Добавить основной сервис"

--- stats/client/src/main/java/ru/practicum/StatsClient.java
+++ stats/client/src/main/java/ru/practicum/StatsClient.java
@@ -1,6 +1,9 @@
-import lombok.RequiredArgsConstructor;
+package ru.practicum;
+
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
@@ -14,25 +17,34 @@ import java.util.List;
 import java.util.Map;
 
 @Component
-@RequiredArgsConstructor
 @Slf4j
 public class StatsClient {
     private final RestTemplate restTemplate;
-    @Value("${statsServerUrl}")
-    private String statsServer;
+    private final String statsServer;
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
+    @Autowired
+    public StatsClient(@Value("${stats-server.url}") String statsServer, RestTemplateBuilder builder) {
+        this.restTemplate = builder.build();
+        this.statsServer = statsServer;
+    }
+
+    public ResponseEntity<List<StatsDtoForView>> getStats(List<String> uris) {
+        return getStats(LocalDateTime.of(2000, 1, 1, 0, 0, 0),
+                LocalDateTime.now(), uris, false);
+    }
+
     public ResponseEntity<List<StatsDtoForView>> getStats(LocalDateTime start, LocalDateTime end,
-                                                          String[] uris, boolean unique) {
+                                                          List<String> uris, boolean unique) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<?> requestEntity = new HttpEntity<>(headers);
         StringBuilder urisForExchange = new StringBuilder();
-        for (int i = 0; i < uris.length; i++) {
-            if (i < (uris.length - 1)) {
-                urisForExchange.append("uris").append("=").append(uris[i]).append(",");
+        for (int i = 0; i < uris.size(); i++) {
+            if (i < (uris.size() - 1)) {
+                urisForExchange.append("uris").append("=").append(uris.get(i)).append(",");
             } else {
-                urisForExchange.append("uris").append("=").append(uris[i]);
+                urisForExchange.append("uris").append("=").append(uris.get(i));
             }
         }
         Map<String, Object> uriVariables = Map.of(
@@ -41,21 +53,21 @@ public class StatsClient {
                 "uris", urisForExchange.toString(),
                 "unique", unique);
 
-        String uri = statsServer + "/stats?start={start}&end={end}&{uris}&{unique}";
+        String uri = statsServer + "/stats?start={start}&end={end}&uris={uris}&unique={unique}";
         log.info("** GET STATS: **\t\t{}", uri);
-        ParameterizedTypeReference<List<StatsDtoForView>> parTypeRef =
-                new ParameterizedTypeReference<>() {
-                };
+
         ResponseEntity<List<StatsDtoForView>> response = restTemplate.exchange(uri, HttpMethod.GET, requestEntity,
-                parTypeRef, uriVariables);
+                new ParameterizedTypeReference<List<StatsDtoForView>>() {
+                },
+                uriVariables);
+
         log.info(response.toString());
         return response;
     }
-
     public void save(StatsDtoForSave statsDtoForSave) {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<StatsDtoForSave> requestEntity = new HttpEntity<>(statsDtoForSave, httpHeaders);
-        restTemplate.exchange(statsServer + "hit", HttpMethod.POST, requestEntity, StatsDtoForSave.class);
+        restTemplate.exchange(statsServer + "/hit", HttpMethod.POST, requestEntity, StatsDtoForSave.class);
     }
 }
